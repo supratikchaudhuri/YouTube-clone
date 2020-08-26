462d94029b25e7664fe4276b8b46dee5c7cb1809
Supratik <rikc.2000@gmail.com>
2020-08-26 23:37:49 +0530

    uploading videos

--- src/YT.js
+++ src/YT.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import "./YT.css"
+import React from 'react';
+import './YT.css';
 import MenuIcon from '@material-ui/icons/Menu';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 
 function YT() {
-    return (
-        <div className="YT">
-            <MenuIcon className="icon"/>
-            <div className="yt">
-                <YouTubeIcon className="icon"/>
-			    <h2>YouTube</h2>
-            </div>
-        </div>
-    )
+	return (
+		<div className="YT">
+			<MenuIcon className="icon" />
+			<div className="yt">
+				<YouTubeIcon className="icon" />
+				<h2>YouTube</h2>
+			</div>
+		</div>
+	);
 }
 
-export default YT
+export default YT;
